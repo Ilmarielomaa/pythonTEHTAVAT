62b67cae153f0e47802e89fdd4fee20171ef0b59
Ilmarielomaa <ilmari.elomaa03@gmail.com>
2023-08-23 14:21:06 +0300

    tehtäviä

---
+++ Moduuli 3/3.1.py
@@ -0,0 +1,5 @@
+kuha = int(input("Kuhan pituus: "))
+raja = int(37)
+
+if kuha < 37:
+    print("Kuha on vielä liian pieni. Laske se takaisin. Kuhan pituudesta puuttuu: " + str(raja - kuha) + " cm.")
