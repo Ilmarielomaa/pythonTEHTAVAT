3838ea61ed8bf0b884ef7c649f3e1dbc4193edfd
Ilmarielomaa <ilmari.elomaa03@gmail.com>
2023-08-23 14:28:14 +0300

    tehtäviä

---
+++ Moduuli 3/3.2.py
@@ -0,0 +1,16 @@
+luokka = input("Anna hyttiluokka: ")
+
+if luokka == "LUX":
+    print("LUX on parvekkeellinen hytti yläkannella. ")
+
+if luokka == "A":
+    print("A on ikkunallinen hytti autokannen yläpuolella. ")
+
+if luokka == "B":
+    print("B on ikkunaton hytti autokannen yläpuolella. ")
+
+if luokka == "C":
+    print("C on ikkunaton hytti autokannen alapuolella. ")
+
+else:
+    print("Virheellinen hyttiluokka")
