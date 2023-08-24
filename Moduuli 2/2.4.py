dca1a9dad29273bc45eeba2ea3ac0a495f3fb990
Ilmarielomaa <ilmari.elomaa03@gmail.com>
2023-08-24 15:07:49 +0300

    tehtäviä

--- Moduuli 2/2.4.py
+++ Moduuli 2/2.4.py
@@ -2,9 +2,10 @@ luku1 = int(input("1 numero: "))
 luku2 = int(input("2 numero: "))
 luku3 = int(input("3 numero: "))
 
-print("summa:")
-print(luku1 + luku2 + luku3)
-print("tulo:")
-print(luku1 * luku2 * luku3)
+print("summa:" + "\n" + str(luku1 + luku2 + luku3))
+
+
+print("tulo:" + "\n" + str(luku1 * luku2 * luku3))
+
 print("keskiarvo:")
 print((luku1 + luku2 + luku3) / 3)
