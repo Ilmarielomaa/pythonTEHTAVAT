4d1f18410b8de3f99a8aec2d29f24f2ace976779
Ilmarielomaa <ilmari.elomaa03@gmail.com>
2023-08-25 15:04:12 +0300

    Tehtävä 5.1

--- Moduuli 5/5.1.py
+++ Moduuli 5/5.1.py
@@ -1,3 +1,21 @@
 import random
 
-Arpakuutioiden lukumäärä = input("Anna arpakuutioiden lukumäärä: ")
+arpakuutiolista = []
+i = 0
+summa = 0
+
+arpakuutio = int(input("Syötä arpakuutioiden määrä: "))
+
+while i < arpakuutio:
+    j = random.randint(1,6)
+    arpakuutiolista.append(j)
+    i += 1
+
+for x in arpakuutiolista:
+    print(x)
+    summa += x
+
+print("Arpakuutioiden lukumäärä on: " + str(summa))
+
+
+
