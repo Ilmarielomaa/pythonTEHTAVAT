f08e18bd7939bfbec5a8127713715de7ab371989
Ilmarielomaa <ilmari.elomaa03@gmail.com>
2023-09-01 13:17:56 +0300

    Tehtävä 2.3

--- Moduuli 2/2.3.py
+++ Moduuli 2/2.3.py
@@ -4,7 +4,7 @@ korkeus = float(input("Syötä suorakulmion korkeus: "))
 print("Tässä vastaus!")
 
 
-print ("piiri: " + str(kanta + korkeus * 2))
+print ("piiri: " + str((kanta + korkeus) * 2))
 print ("pinta-ala: " + str(korkeus * kanta))
 
 
