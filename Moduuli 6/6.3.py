3e37dffe4888b0b61dc94fc8a5e94cc26ece8bfb
Ilmarielomaa <ilmari.elomaa03@gmail.com>
2023-09-05 16:41:43 +0300

    tehtävä 6.3

---
+++ Moduuli 6/6.3.py
@@ -0,0 +1,8 @@
+def muuttuja(gal):
+    gallona = float(3.785)
+    bensiini = gal * gallona
+    print(f"{gal} gallonaa on {bensiini} litraa.")
+    return
+
+maara = float(input())
+muuttuja(maara)
