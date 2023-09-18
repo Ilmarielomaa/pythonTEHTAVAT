15be3c3990849cbc0d782fc2b4a464b584068b60
Ilmarielomaa <ilmari.elomaa03@gmail.com>
2023-09-18 11:05:49 +0300

    tehtävä 9.1

---
+++ Moduuli 9/9.1.py
@@ -0,0 +1,21 @@
+class auto:
+    def __init__(self, rekisteritunnus, huippunopeus):
+        self.rekisteritunnus = rekisteritunnus
+        self.huippunopeus = huippunopeus
+        self.tämänhetkinen_nopeus = 0
+        self.kuljettu_matka = 0
+
+    def tulosta_ominaisuudet(self):
+        print("Auton tiedot:")
+        print(f"rekisteritunnus: {self.rekisteritunnus}")
+        print(f"huippunopeus: {self.huippunopeus} km/h")
+        print(f"tämänhetkinen_nopeus: {self.tämänhetkinen_nopeus} km/h")
+        print(f"kuljettu_matka: {self.kuljettu_matka} km")
+
+def main():
+    uusi_auto = auto("ABC-123", 142)
+    uusi_auto.tulosta_ominaisuudet()
+
+if __name__ == "__main__":
+    main()
+
